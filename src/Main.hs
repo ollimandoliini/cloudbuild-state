695bde469f855950ea88510e24694671e562a52c
Olli Kortelahti <okortelahti@gmail.com>
2021-10-25 19:22:50 +0300

    Add locking for db file

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy.Char8 as B (putStrLn)
 import qualified Data.Map as Map
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
+import System.FileLock
 
 dbPath = "db.json"
 
@@ -24,33 +25,26 @@ handleArgs args = putStr $ "Unknown command: '" ++ unwords args ++ "'\nCommands:
 
 get :: String -> IO ()
 get key = do
-  fileExists <- doesFileExist dbPath
-  if fileExists
-    then
-      B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) >>= Map.lookup key of
-        (Just val) -> putStrLn val
-        Nothing -> error "Value not found"
-    else error "Value not found"
+  lockFile dbPath Exclusive
+  B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) >>= Map.lookup key of
+    (Just val) -> putStrLn val
+    Nothing -> error "Value not found"
 
 set :: String -> String -> IO ()
 set key val = do
-  fileExists <- doesFileExist dbPath
-  if fileExists
-    then
-      B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) of
-        (Just db) -> B.writeFile dbPath $ encode $ Map.insert key val db
-        Nothing -> B.writeFile dbPath (encode $ Map.fromList [(key, val)])
-    else B.writeFile dbPath (encode $ Map.fromList [(key, val)])
+  lockFile dbPath Exclusive
+  B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) of
+    (Just db) -> B.writeFile dbPath $ encode $ Map.insert key val db
+    Nothing -> B.writeFile dbPath (encode $ Map.fromList [(key, val)])
 
 list :: IO ()
 list = do
-  fileExists <- doesFileExist dbPath
-  when fileExists $
-    B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) of
-      Just db -> forM_ (Map.toList db) (\(key, val) -> putStrLn (key ++ "=" ++ val))
-      Nothing -> return ()
+  lockFile dbPath Exclusive
+  B.readFile dbPath >>= \content -> case (decode content :: Maybe Database) of
+    Just db -> forM_ (Map.toList db) (\(key, val) -> putStrLn (key ++ "=" ++ val))
+    Nothing -> return ()
 
 listJson :: IO ()
 listJson = do
-  fileExists <- doesFileExist dbPath
-  when fileExists (B.readFile dbPath >>= B.putStrLn)
+  lockFile dbPath Exclusive
+  B.readFile dbPath >>= B.putStrLn
